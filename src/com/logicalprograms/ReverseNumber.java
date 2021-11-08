59a66ead1322a4721404db2bc17b981d4b2c646e
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-08 21:43:01 +0530

    reading input value using scanner

---
+++ src/com/logicalprograms/ReverseNumber.java
@@ -0,0 +1,16 @@
+package com.logicalprograms;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter a number : ");
+		int num = scan.nextInt();
+
+	}
+
+}

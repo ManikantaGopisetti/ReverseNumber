e1c1c27b415c8de5f83523a6cb81c4bbe07c40d6
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-08 21:45:45 +0530

    finding reverse of given number using while condition

--- src/com/logicalprograms/ReverseNumber.java
+++ src/com/logicalprograms/ReverseNumber.java
@@ -11,6 +11,15 @@ public class ReverseNumber {
 		System.out.println("Enter a number : ");
 		int num = scan.nextInt();
 
+		int revnum = 0;
+		int rem = 0;
+
+		while (num > 0) {
+			rem = num % 10;
+			revnum = revnum * 10 + rem;
+			num = num / 10;
+		}
+		System.out.println("Reverse of the given number : " + revnum);
 	}
 
 }
